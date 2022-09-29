434bfe5219e49795a9344dc70038bdfda3987283
IshaqKassam <ishaq.kassam@gmail.com>
2022-09-29 20:25:45 +0300

    done the form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+	
+
+
+	return (
+		<div className="App">
+			<Form>
+				<label>sample frequency</label>
+				<input min="0" type="number" />
+				<label>max frequency</label>
+				<input min="0" type="number" />
+				<label>min frequency</label>
+				<input min="0" type="number" />
+				<label>bit depth</label>
+				<input min="0" type="number" />
+				<label>quantization levels</label>
+				<input min="0" type="number" />
+				<label>sample size</label>
+				<input min="0" type="number" />
+				<label>plain index</label>
+				<input min="0" type="number" />
+				<p>Quantized Value</p>
+				<p>Quantization Value</p>
+
+				<button>Submit</button>
+			</Form>
+		</div>
+	)
 }
 
-export default App;
+export default App
+
+const Form = styled.div`
+	margin: 100px auto;
+	width: 25%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+
+	input {
+		height: 25px;
+		padding: 5px;
+		margin-bottom: 10px;
+		font-size: 24px;
+	}
+
+	label {
+		font-size: 24px;
+	}
+
+	button {
+		padding: 10px;
+		font-size: 24px;
+		cursor: pointer;
+	}
+`
